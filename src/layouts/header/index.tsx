194431ae26b287924fc1a19344af8ca6dd992483
Sebastien Cloiseau <sebastien.cloiseau@gmail.com>
2025-09-01 16:33:02 +0200

    fix: header wide screen

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -2,8 +2,8 @@ import { WalletConnection } from '@/components/connect-wallet';
 
 export const Header = () => {
   return (
-    <header className="max-w-screen-2xl flex justify-center px-8 py-4 w-full bg-secondary  border-b border-borderColor mb-4">
-      <div className="w-full flex items-center justify-between relative mx-auto">
+    <header className=" flex justify-center px-8 py-4 w-full bg-secondary border-b border-borderColor mb-4">
+      <div className="w-full flex items-center justify-between relative mx-auto max-w-screen-2xl">
         <h1 className="text-2xl font-bold">Nad.fun</h1>
         <WalletConnection />
       </div>
