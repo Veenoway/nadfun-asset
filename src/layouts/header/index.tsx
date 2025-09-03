b565dad302f78ed7e256ed19099ee3598d278a31
velikanghost <velikanghost@yahoo.com>
2025-09-03 22:05:22 +0100

    config

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="flex justify-center px-8 py-4 w-full">
       <div className="w-full flex items-center justify-between relative mx-auto max-w-screen-2xl">
-        <h1 className="text-2xl font-bold text-white/90">Nad.fun</h1>
+        <h1 className="text-2xl font-bold text-white/80">Nad.fun</h1>
         <WalletConnection />
       </div>
     </header>
